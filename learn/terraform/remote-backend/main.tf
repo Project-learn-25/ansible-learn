2af2bdeee9f50f4556dd34e2e7721742fc360bee
missjan1595 <missjan1595@gmail.com>
2025-02-04 22:42:47 +0530

    up

--- learn/terraform/remote-backend/main.tf
+++ learn/terraform/remote-backend/main.tf
@@ -7,7 +7,7 @@ aws = {
 }
 
 
-resource "aws_s3_bucket" "example"{
+backend "aws_s3_bucket" "example"{
     bucket = "my-tf-test-15952521"
     key = "sm/myfile/terraform.tfstate"
     region = "us-east-1"
