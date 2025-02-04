e812dcf0302ee3df00e1da93e3ad1069d68558f0
missjan1595 <missjan1595@gmail.com>
2025-02-04 21:15:35 +0530

    up

---
+++ learn/terraform/conditions/main.tf
@@ -0,0 +1,8 @@
+# ? and :
+
+resource "aws_instance" "test" {
+    ami = "ami-04b4f1a9cf54c11d0" 
+    instance_type = "var.instance_type" == "t2*" ? "t3.micro" : var.instance_type
+}
+
+variab "instance_type" {}
