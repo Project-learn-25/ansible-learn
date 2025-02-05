68cb37c3acc48311a16dce335b7cbefa8558cd9e
missjan1595 <missjan1595@gmail.com>
2025-02-05 20:36:19 +0530

    up

---
+++ learn/terraform/tf-project/variables.tf
@@ -0,0 +1,6 @@
+#aws region
+variable "aws_region" {
+    description = "Region in which AWS resources to be created"
+    type = string
+    default = "us-east-1"
+}
---
+++ learn/terraform/tf-project/version.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_version = ">= 1.0" #which means any version equal
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+#provier block
+provide "aws" {
+    region = var.aws_region
+}
