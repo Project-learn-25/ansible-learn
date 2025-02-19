8a46d62130e199fa88d9f65250d22060d641785f
missjan1595 <missjan1595@gmail.com>
2025-02-19 12:17:09 +0530

    up

---
+++ learn/python/demo.py
@@ -0,0 +1,32 @@
+print ("hello executed directly print")
+
+#def greet():
+    #return ("hello from module")
+
+    #arguments passing 
+
+    #import sys
+    #print(f"script name :{sys.argv[0]}")
+    #print(f"first argument :{sys.argv[1]}")
+
+    #for complex argument 
+
+    #import argparse
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("name" , help="enter your name")
+    #args = parser.parser_args()
+    #print(f"hello,{args.name}") 
+    
+#schedule job
+
+#import schedule
+#import time
+
+#def job():
+    #print("Running scheduled job...")
+
+#schedule.every(5).seconds.do(job)
+
+#while True:
+    #schedule.run_pending()
+    #time.sleep(1)
